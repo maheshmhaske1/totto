7ae8b86903ccd1ab3d4bbdaf381c1e865f3912cf
Mahesh mhaske <70438274+maheshmhaske1@users.noreply.github.com>
2023-12-01 21:03:59 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 exports.dbConnection = async () => {
-    mongoose.connect(process.env.DATABASE, (error) => {
+    mongoose.connect("mongodb+srv://mahes:Mahi7558@cluster0.4rkar.mongodb.net/ecommerce", (error) => {
         if (!error) {
             console.log('Connected with MongoDB...')
         }
